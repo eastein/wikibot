2978f9fb54c6c30eb65ef5c0981c549aea411844
Eric Stein <toba@des.truct.org>
2014-12-11 00:08:39 -0600

    improve the code for shortening and make it so that if it blows up, we still have a working bot. Also make it so that it takes --gapi-key instead of heaping more mistakes on the hahaha argv train.

--- wikibot.py
+++ wikibot.py
@@ -4,45 +4,53 @@ import wikichanges
 import mediorc
 import time
 
-class WikiBot(mediorc.IRC) :
-	def __init__(self, server, nick, chan, wc) :
-		self.last_polled = 0
-		self.wc = wc
-		mediorc.IRC.__init__(self, server, nick, chan)
 
-	def do_work(self) :
-		now = time.time()
-		if now < self.last_polled + 2224 :
-			return
+POLL_TIME = 10
 
-		#try :
-		for msg in self.wc.poll() :
-			self.connection.privmsg(self._chan, msg)
-		# except : sfjslfdkj
-		
-		self.last_polled = now
+class WikiBot(mediorc.IRC):
 
-class WikiBotThread(mediorc.IRCThread) :
-	def __init__(self, server, nick, chan, wc) :
-		self.bot_create = lambda: WikiBot(server, nick, chan, wc)
-		mediorc.IRCThread.__init__(self)
+    def __init__(self, server, nick, chan, wc):
+        self.last_polled = 0
+        self.wc = wc
+        mediorc.IRC.__init__(self, server, nick, chan)
+
+    def do_work(self):
+        now = time.time()
+        if now < self.last_polled + POLL_TIME :
+            return
+
+        # try :
+        for msg in self.wc.poll():
+            self.connection.privmsg(self._chan, msg)
+        # except : sfjslfdkj
+
+        self.last_polled = now
 
 
-if __name__ == '__main__' :
-	parser = optparse.OptionParser()
+class WikiBotThread(mediorc.IRCThread):
 
-	(options, args) = parser.parse_args()
+    def __init__(self, server, nick, chan, wc):
+        self.bot_create = lambda: WikiBot(server, nick, chan, wc)
+        mediorc.IRCThread.__init__(self)
 
-	s,n,c,u,k = args
 
-	wc = wikichanges.WikiChanges(u, emit_start=False, max_age=datetime.timedelta(hours=1), gapi_key=k)
+if __name__ == '__main__':
+    parser = optparse.OptionParser()
+    parser.add_option('--gapi-key', dest='gapi_key', default=None,
+                      help="GAPI key to use to shorten URLs. Skip it if you don't want URL shortening.")
+    (options, args) = parser.parse_args()
 
-	try :
-		s = WikiBotThread(s,n,c,wc)
-	except IndexError :
-		print 'usage: wikibot.py server nick channel atom_url'
-		sys.exit(1)
-	
-	# threading,? NOPE
-	s.run()
+    s, n, c, u = args
 
+    k = options.gapi_key
+
+    wc = wikichanges.WikiChanges(u, emit_start=False, max_age=datetime.timedelta(hours=1), gapi_key=k)
+
+    try:
+        s = WikiBotThread(s, n, c, wc)
+    except IndexError:
+        print 'usage: wikibot.py server nick channel atom_url'
+        sys.exit(1)
+
+    # threading,? NOPE
+    s.run()
--- wikichanges.py
+++ wikichanges.py
@@ -7,58 +7,62 @@ import pytz
 import datetime
 import json
 
+
 class WikiChanges(object):
-	def __init__(self, url, emit_start=False, max_age=None, gapi_key=None):
-		self.url = url
-		self.synced = False
-		self.messages = set()
-		self.emit_start = emit_start
-		self.max_age = max_age
-		self.key = gapi_key
 
-	def shorten(self, url):
-                if not self.key:
-                    return url
+    def __init__(self, url, emit_start=False, max_age=None, gapi_key=None):
+        self.url = url
+        self.synced = False
+        self.messages = set()
+        self.emit_start = emit_start
+        self.max_age = max_age
+        self.key = gapi_key
 
-		shorten_url = 'https://www.googleapis.com/urlshortener/v1/url'
-		qs = '?key=' + self.key
-                headers = {'content-type': 'application/json'}
-                data = json.dumps({ 'longUrl': url })
-		res = requests.post(shorten_url + qs, data=data, headers=headers).text
-		return json.loads(res)['id']
+    def shorten(self, url):
+        if not self.key:
+            return url
 
+        try :
+            shorten_url = 'https://www.googleapis.com/urlshortener/v1/url'
+            qs = '?key=' + self.key
+            headers = {'content-type': 'application/json'}
+            data = json.dumps({'longUrl': url})
+            res = requests.post(shorten_url + qs, data=data, headers=headers).text
+            return json.loads(res)['id']
+        except :
+            # it's url shortening. what do you want? I'm sorry that I'm not syslogging this. So very sorry.
+            # I lied, I'm not sorry.
+            return url
 
-	def poll(self):
-		r = requests.get(self.url)
-		d = xmltodict.parse(r.text)
-		entries = d['feed']['entry']
-		entries.reverse()
-		now = datetime.datetime.utcnow()
-		now = now.replace(tzinfo=pytz.utc, microsecond=0)
-		for e in entries:
-			url = e['link']['@href']
-			name = e['author']['name']
-			title = e['title']
-			updated = isodate.parse_datetime(e['updated'])
-			age = now - updated
-			if url not in self.messages:
-				age_str = 'just now'
-				if age > datetime.timedelta(minutes=3):
-					age_str = '%s ago' % str(age)
-				msg = '%s edited "%s" %s %s' % (name, title, age_str, self.shorten(url))
-				self.messages.add(url)
-				if self.synced or self.emit_start:
-					if self.max_age is not None:
-						if age > self.max_age:
-							continue
-					yield msg
-		self.synced = True
+    def poll(self):
+        r = requests.get(self.url)
+        d = xmltodict.parse(r.text)
+        entries = d['feed']['entry']
+        entries.reverse()
+        now = datetime.datetime.utcnow()
+        now = now.replace(tzinfo=pytz.utc, microsecond=0)
+        for e in entries:
+            url = e['link']['@href']
+            name = e['author']['name']
+            title = e['title']
+            updated = isodate.parse_datetime(e['updated'])
+            age = now - updated
+            if url not in self.messages:
+                age_str = 'just now'
+                if age > datetime.timedelta(minutes=3):
+                    age_str = '%s ago' % str(age)
+                msg = '%s edited "%s" %s %s' % (name, title, age_str, self.shorten(url))
+                self.messages.add(url)
+                if self.synced or self.emit_start:
+                    if self.max_age is not None:
+                        if age > self.max_age:
+                            continue
+                    yield msg
+        self.synced = True
 
 if __name__ == '__main__':
-	wc = WikiChanges(sys.argv[1], emit_start=True, max_age=datetime.timedelta(hours=2))
-	while True:
-		for msg in wc.poll():
-			print msg
-		time.sleep(60)
-	
-	
+    wc = WikiChanges(sys.argv[1], emit_start=True, max_age=datetime.timedelta(hours=2))
+    while True:
+        for msg in wc.poll():
+            print msg
+        time.sleep(60)
